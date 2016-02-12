bcc54239e3fd1f2a507b0401e0e7c87d80f7276f
Marty Schoch <marty.schoch@gmail.com>
2016-02-12 15:25:28 -0500

    switch to couchbaselabs fork of c-snappy

--- cgo_flags.go
+++ cgo_flags.go
@@ -4,11 +4,11 @@ package forestdb
 
 import (
 	// explicit because these Go libraries do not export any Go symbols.
-	_ "github.com/cockroachdb/c-snappy"
+	_ "github.com/couchbaselabs/c-snappy"
 )
 
 // #cgo CPPFLAGS: -Iinternal/include -Iinternal/src -Iinternal/option -Iinternal/utils
-// #cgo CPPFLAGS: -I../../cockroachdb/c-snappy/internal
+// #cgo CPPFLAGS: -I../../couchbaselabs/c-snappy/internal
 // #cgo CPPFLAGS: -DSNAPPY
 // #cgo CXXFLAGS: -std=c++11 -DHAVE_GCC_ATOMICS -fno-omit-frame-pointer -momit-leaf-frame-pointer
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
